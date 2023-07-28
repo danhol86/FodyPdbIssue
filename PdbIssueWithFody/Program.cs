76c3a96ca25bd11cf8743386a96f38b85d46e9ac
Dan Holgate <Dan.Holgate@agileautomations.co.uk>
2023-07-28 11:36:04 +0100

    .

---
+++ PdbIssueWithFody/Program.cs
@@ -0,0 +1,15 @@
+﻿using Shared;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace PdbIssue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestCode.Test();
+        }
+    }
+}
---
+++ Shared/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Shared
+{
+    public class TestCode
+    {
+        public static void Test()
+        {
+            var myassembly = Assembly.GetEntryAssembly().Location;
+            var mypdb = myassembly.Replace(".dll", ".pdb");
+
+
+            var pdbtext = File.ReadAllText(myassembly);
+
+            var containsnewpath = pdbtext.Contains("MyNewPath");
+
+            Console.WriteLine(containsnewpath ? "YAY!" : "BOO!");
+
+            Console.ReadLine();
+        }
+    }
+}
